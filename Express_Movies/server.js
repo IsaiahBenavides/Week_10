ef5a8362ce9cbbef4673174ea8fae0c261b281aa
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-30 10:37:45 -0500

    Requires set up in server

---
+++ Express_Movies/server.js
@@ -0,0 +1,26 @@
+require(`dotenv`).config()
+const express = require(`express`)
+const morgan = require(`morgan`)
+const methodOverride= require(`method-override`)
+const mongoose = require(`mongoose`)
+const path = require(`path`)
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Extablish connection
+mongoose.connect(DATABASE_URL, CONFIG)
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
