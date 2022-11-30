5925ef6730990392cd82f08a6c83ea1be16a1c7a
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-30 11:04:35 -0500

    Server "boiler plate"

--- Express_Movies/server.js
+++ Express_Movies/server.js
@@ -24,3 +24,55 @@ mongoose.connection
   .on("open", () => console.log("Connected to Mongoose"))
   .on("close", () => console.log("Disconnected from Mongoose"))
   .on("error", (error) => console.log(error));
+
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+
+// pull schema and model from mongoose using object destructuring
+const { Schema, model } = mongoose;
+
+// make Movies schema
+const MoviesSchema = new Schema({
+  title: {type: String, required: true},
+  releaseDate: String,
+  length: Number,
+  genre: String,
+  poster: {type: String, required: true},
+  director: {type: String, required: true},
+  rating: String,
+  watchAgain: Boolean,
+  cast: [{type: String}]
+});
+
+// make Movie model
+const Movie = model("Movie", MoviesSchema);
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = express()
+app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx')
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("your server is running... better catch it.");
+});
+
+  //////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
