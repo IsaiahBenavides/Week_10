28aa7cadcd1a95c63d8a4b737b44609cd32a9d23
Isaiah Bens <benavidesisaiah@outlook.com>
2022-12-01 12:19:31 -0500

    Warning fixes

--- Express_Movies/views/Default.jsx
+++ Express_Movies/views/Default.jsx
@@ -5,8 +5,8 @@ class DefaultLayout extends React.Component {
         return(
             <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>My Movie Website</title>
     {/* Milligram CSS for Some Default Styling */}
@@ -32,7 +32,7 @@ class DefaultLayout extends React.Component {
     <script
       src="https://code.jquery.com/jquery-3.6.0.min.js"
       integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-      crossorigin="anonymous"
+      crossOrigin="anonymous"
     ></script>
 
     {/* OUR CSS AND JS */}
